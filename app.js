fb289d091f51ff8527c99bbdc7e7a9b6189860dd
Rengkat <successinmotion11@gmail.com>
2024-07-14 14:05:08 +0100

    error hidleware

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
+require("express-async-errors");
+const port = process.env.PORT || 5000;
 
 //express
 const express = require("express");
 const app = express();
+//database
+const connectDB = require("./db/connect");
 //midlewares
-
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+app.use(express.json());
 //routes
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 //errors
-const port = process.env.PORT || 5000;
+app.use(notFoundMiddleware);
+app.use(errorHandler);
 
-app.listen(() => console.log("Server listening at prt 5000"));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => console.log(`Server listening at port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
